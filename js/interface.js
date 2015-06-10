4b9c93743883f19654e873a30bb7c0844cb27af9
Matteo Manzo <imatteo.manzo@gmail.com>
2015-06-10 21:45:26 +0100

    edited jquery function and made it responsive

--- js/interface.js
+++ js/interface.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	$('#projects').on('click', function(){
-		$('#slider #slides').animate({'margin-left': '-60.05em'}, 1000);
+		$('#slider #slides').animate({'margin-left': '-100%'}, 1000);
 	});
-	
+
 	$('#contact').on('click', function(){
-		$('#slider #slides').animate({'margin-left': '-120.10em'}, 1000);
+		$('#slider #slides').animate({'margin-left': '-200%'}, 1000);
 	});
-	
+
 	$('#about').on('click', function(){
 		$('#slider #slides').animate({'margin-left': '0em'}, 1000);
 	})
